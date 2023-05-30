b0dd84e16869a64dee6287c14ad014df8f117afc
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-05-30 18:04:43 -0500

    refactor(offers): :rocket: base url fix

--- lib/common/api_config/api_config.dart
+++ lib/common/api_config/api_config.dart
@@ -1,3 +1,4 @@
 class ApiConfig {
-  static String baseUrl = 'https//example.com/';
+  //static String baseUrl = 'https//example.com/';
+  static String baseUrl = 'https://my-json-server.typicode.com/LITO-TR/fake-api/';
 }
