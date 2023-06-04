1a34696fea163e310bd070ebc1c43febf7faa722
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-06-04 17:20:29 -0500

    added offer facade service

--- lib/offers/application/offer_facade_service.dart
+++ lib/offers/application/offer_facade_service.dart
@@ -2,10 +2,8 @@ import '../domain/entities/offer.dart';
 import '../infrastructure/repositories/offer_repository.dart';
 
 class OfferFacadeService {
-  OfferFacadeService({
-    required this.repository
-});
-final OfferRepository repository;
+  OfferFacadeService({required this.repository});
+  final OfferRepository repository;
   Future<List<Offer>> getAllOffers() async {
     return repository.getAllOffers();
   }
