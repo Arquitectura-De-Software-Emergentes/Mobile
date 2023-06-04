3926ca281c796e3f8e430aaef4040bcb910d0d75
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-06-04 17:22:18 -0500

    refactor(offers): :ambulance: Widget refactored to stateless with bloc

--- lib/offers/presentation/offers_list/offers_list_screen.dart
+++ lib/offers/presentation/offers_list/offers_list_screen.dart
@@ -1,88 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:teacher_finder/common/widgets/app_bar_custom.dart';
-import 'package:teacher_finder/offers/infrastructure/repositories/offer_repository.dart';
+import 'package:teacher_finder/offers/presentation/offers_list/bloc/bloc.dart';
 import 'package:teacher_finder/offers/presentation/offers_list/widgets/filter_button.dart';
 import '../../../common/widgets/offer_card.dart';
 import '../../domain/entities/offer.dart';
-import '../../infrastructure/data_sources/offer_remote_data_provider.dart';
 import '../offer_detail/offer_detail_screen.dart';
 import 'widgets/search_bar_custom.dart';
 
-class OffersListScreen extends StatefulWidget {
+class OffersListScreen extends StatelessWidget {
   const OffersListScreen({Key? key}) : super(key: key);
 
   @override
-  State<OffersListScreen> createState() => _OffersListScreenState();
-}
-
-class _OffersListScreenState extends State<OffersListScreen> {
-  late Future<List<Offer>> _offersList;
-
-  @override
-  void initState() {
-    final OfferRemoteDataProvider offerRemoteDataProvider =
-        OfferRemoteDataProvider();
-    final offerRepository =
-        OfferRepository(offerRemoteDataProvider: offerRemoteDataProvider);
-    _offersList = offerRepository.getAllOffers();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const AppBarCustom(isHome: true),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 25),
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Flexible(
-                    child: SizedBox(
+    return BlocProvider<OffersListBloc>(
+      create: (BuildContext context) => OffersListBloc()..getAllOffers(),
+      child: Scaffold(
+        appBar: const AppBarCustom(isHome: true),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 25),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Flexible(
+                      child: SizedBox(
                         width: MediaQuery.of(context).size.width * 0.7,
-                        child: const SearchBarCustom()),
-                  ),
-                  const FilterButton(),
-                ],
+                        child: const SearchBarCustom(),
+                      ),
+                    ),
+                    const FilterButton(),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: FutureBuilder<List<Offer>>(
-              future: _offersList,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No offers found'));
-                } else {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      Offer offer = snapshot.data![index];
-                      return OfferCard(
+            Expanded(
+              child: BlocBuilder<OffersListBloc, OffersListState>(
+                builder: (context, state) {
+                  if (state is OffersListLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is OffersListLoaded) {
+                    return ListView.builder(
+                      itemCount: state.offersList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        Offer offer = state.offersList[index];
+                        return OfferCard(
                           offer: offer,
                           onPress: () {
-                            goToOfferDetail(offer);
-                          });
-                    },
-                  );
-                }
-              },
+                            goToOfferDetail(context, offer);
+                          },
+                        );
+                      },
+                    );
+                  } else if (state is OffersListError) {
+                    return const Text('error');
+                  }
+
+                  return Container();
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
-  void goToOfferDetail(Offer offer) {
+  void goToOfferDetail(BuildContext context, Offer offer) {
     Navigator.push(
         context,
         MaterialPageRoute(
