0dd6d7f06daddf5e72d92e259acfe013cdae32af
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-06-07 19:50:43 -0500

    refactor(teacher finder): :memo: refactor leading and name correction

--- lib/common/widgets/custom_app_bar.dart
+++ lib/common/widgets/custom_app_bar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:teacher_finder/common/styles/styles.dart';
 
-class AppBarCustom extends StatelessWidget implements PreferredSizeWidget {
-  const AppBarCustom({super.key, required this.isHome});
-  final bool isHome;
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
@@ -12,17 +12,6 @@ class AppBarCustom extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       title: const Text('Teacher Finder'),
       backgroundColor: Styles.primaryColor,
-      leading: isHome
-          ? IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {},
-            )
-          : IconButton(
-              icon: const Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
       actions: const [
         Padding(
           padding: EdgeInsets.only(right: 10),
