1f8743e83d28c19181f5bab51ca1ff3522b84dcf
Cristhian Orosco <u201921031@upc.edu.pe>
2023-06-07 20:48:14 -0500

    feat(offers): add navigation create new offer

--- lib/offers/presentation/my_offers_list/my_offers_list_screen.dart
+++ lib/offers/presentation/my_offers_list/my_offers_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:teacher_finder/common/widgets/custom_app_bar.dart';
 import 'package:teacher_finder/common/widgets/custom_drawer.dart';
+import 'package:teacher_finder/offers/presentation/new_offer/new_offert.dart';
 
 import '../../../common/widgets/offer_card.dart';
 import '../../domain/entities/offer.dart';
@@ -69,7 +70,10 @@ class _MyOffersListScreenState extends State<MyOffersListScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Implementation for create new
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: ((context) => const NewOffertPage())));
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add, color: Colors.white),
