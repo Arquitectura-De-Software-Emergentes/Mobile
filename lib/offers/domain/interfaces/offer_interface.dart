8ab165c13ad7ba3b1535cce3ceade87d922154aa
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-06-10 13:22:08 -0500

    feat(offers): :sparkles: added new offer model

--- lib/offers/domain/interfaces/offer_interface.dart
+++ lib/offers/domain/interfaces/offer_interface.dart
@@ -2,4 +2,5 @@ import 'package:teacher_finder/offers/domain/entities/offer.dart';
 
 abstract class OfferInterface {
   Future<List<Offer>> getAllOffers();
+  Future<void> createOffer(Offer offer);
 }
