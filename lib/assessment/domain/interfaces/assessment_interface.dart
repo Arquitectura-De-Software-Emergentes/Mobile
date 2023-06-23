812f5e684aa3bd7187216a477c1a8aebcd7cadbc
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-06-23 00:26:38 -0500

    feat(assessment): :sparkles: added contracts

--- lib/assessment/domain/interfaces/assessment_interface.dart
+++ lib/assessment/domain/interfaces/assessment_interface.dart
@@ -1,5 +1,10 @@
+import '../entities/question.dart';
 import '../entities/test.dart';
 
 abstract class AssessmentInterface {
   Future<Test> createTest(Test test);
+  Future<Test> createQuestion(Question question);
+  Future<List<Test>> getAllTestsByRecruiterId(int recruiterId);
+  //LOCAL :P
+  Future<List<Question>> getAllQuestionsByTestId(int testId);
 }
--- lib/assessment/infrastructure/repositories/assessment_repository.dart
+++ lib/assessment/infrastructure/repositories/assessment_repository.dart
@@ -1,3 +1,4 @@
+import 'package:teacher_finder/assessment/domain/entities/question.dart';
 import 'package:teacher_finder/assessment/domain/entities/test.dart';
 import 'package:teacher_finder/assessment/domain/interfaces/assessment_interface.dart';
 import 'package:teacher_finder/assessment/infrastructure/data_sources/assessment_remote_data_provider.dart';
@@ -5,9 +6,28 @@ import 'package:teacher_finder/assessment/infrastructure/data_sources/assessment
 class AssessmentRepository extends AssessmentInterface {
   AssessmentRepository({required this.assessmentRemoteDataProvider});
   final AssessmentRemoteDataProvider assessmentRemoteDataProvider;
+
+  @override
+  Future<List<Test>> getAllTestsByRecruiterId(int recruiterId) {
+    // TODO: implement getAllTests
+    throw UnimplementedError();
+  }
+
   @override
   Future<Test> createTest(Test test) async {
     final response = await assessmentRemoteDataProvider.createTest(test);
     return response;
+  }
+
+  @override
+  Future<List<Question>> getAllQuestionsByTestId(int testId) {
+    // TODO: implement getAllQuestionsByTestId
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Test> createQuestion(Question question) {
+    // TODO: implement createQuestion
+    throw UnimplementedError();
   }
 }
