f80363d32e54849430d23b5c4a8947b758e8f162
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-06-30 03:05:42 -0500

    fix(teacher finder): :bug: refactored apply

--- lib/appications/infrastructure/applications_remote_data_source.dart
+++ lib/appications/infrastructure/applications_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 import '../../common/api_config/api_config.dart';
@@ -5,10 +7,27 @@ import '../../common/api_config/api_config.dart';
 class ApplicationsRemoteDateSource {
   String endpoint = 'applications';
 
-  Future<void> applyJobOffer(int jobOfferId, int applicantId) async {
+  Future<String> applyJobOffer(int jobOfferId, int applicantId) async {
     try {
       final response = await http.post(Uri.parse(
-          '${ApiConfig.baseUrl}/$endpoint/apply/job-offers/$jobOfferId/aplicants/6'));
+          '${ApiConfig.baseUrl}/$endpoint/apply/job-offers/$jobOfferId/aplicants/$applicantId'));
+      print(response.body);
+      if (response.statusCode == 422) {
+        return 'already applied';
+      } else {
+        return 'successful application';
+      }
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+
+  Future<List<dynamic>> getAllApplicationsByOfferId(int offerId) async {
+    try {
+      final response = await http
+          .get(Uri.parse('${ApiConfig.baseUrl}/$endpoint/job-offers/$offerId'));
+      final List<dynamic> jsonData = jsonDecode(response.body) as List<dynamic>;
+      return jsonData;
     } catch (error) {
       throw Exception(error);
     }
